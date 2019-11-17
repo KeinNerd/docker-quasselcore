4cd60c7af07bc08833cac8731e9687b217001223
KeinNerd <53052025+KeinNerd@users.noreply.github.com>
2019-11-17 14:37:09 +0100

    Update start.sh
    add executable bit so container can start

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ then
     openssl req -x509 -subj "/C=aa/ST=aa/L=a/O=a/CN=a" -nodes -days 365 -newkey rsa:4096 -keyout /config/quasselCert.pem -out /config/quasselCert.pem
 fi
 
-# start quasselcore
+# start quassel-core
 echo "start quasselcore"
 /usr/local/bin/quasselcore --port=4242 --configdir=/config/
